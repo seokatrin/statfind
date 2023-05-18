0e808b9b151a83ec09642a36c2d0295b11ac638f
seokatrin <seokatrin@mail.ru>
2023-05-18 17:55:50 +0500

    init

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,14 +1,4 @@
 self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "/home/esorokina/work/statfind/src/app/layout.tsx": [
-      "/home/esorokina/work/statfind/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
-      "/home/esorokina/work/statfind/src/app/globals.css"
-    ]
-  },
-  "cssModules": {
-    "/home/esorokina/work/statfind/src/app/page": [
-      "/home/esorokina/work/statfind/src/app/globals.css",
-      "/home/esorokina/work/statfind/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
-    ]
-  }
+  "cssImports": {},
+  "cssModules": {}
 }
---
+++ .next/static/chunks/app/favicon.ico/route.js
@@ -0,0 +1,39 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/favicon.ico/route"],{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?absolutePagePath=%2Fhome%2Fesorokina%2Fwork%2Fstatfind%2Fapp%2Ffavicon.ico&page=%2Ffavicon.ico%2Froute!":
+/*!******************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?absolutePagePath=%2Fhome%2Fesorokina%2Fwork%2Fstatfind%2Fapp%2Ffavicon.ico&page=%2Ffavicon.ico%2Froute! ***!
+  \******************************************************************************************************************************************************************************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("\n    (window.__NEXT_P = window.__NEXT_P || []).push([\n      \"/favicon.ico/route\",\n      function () {\n        return __webpack_require__(/*! ./app/favicon.ico */ \"(app-client)/./app/favicon.ico\");\n      }\n    ]);\n    if(true) {\n      module.hot.dispose(function () {\n        window.__NEXT_P.push([\"/favicon.ico/route\"])\n      });\n    }\n  //# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1jbGllbnQtcGFnZXMtbG9hZGVyLmpzP2Fic29sdXRlUGFnZVBhdGg9JTJGaG9tZSUyRmVzb3Jva2luYSUyRndvcmslMkZzdGF0ZmluZCUyRmFwcCUyRmZhdmljb24uaWNvJnBhZ2U9JTJGZmF2aWNvbi5pY28lMkZyb3V0ZSEuanMiLCJtYXBwaW5ncyI6IjtBQUNBO0FBQ0E7QUFDQTtBQUNBLGVBQWUsbUJBQU8sQ0FBQyx5REFBbUI7QUFDMUM7QUFDQTtBQUNBLE9BQU8sSUFBVTtBQUNqQixNQUFNLFVBQVU7QUFDaEI7QUFDQSxPQUFPO0FBQ1A7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvP2M1ZjMiXSwic291cmNlc0NvbnRlbnQiOlsiXG4gICAgKHdpbmRvdy5fX05FWFRfUCA9IHdpbmRvdy5fX05FWFRfUCB8fCBbXSkucHVzaChbXG4gICAgICBcIi9mYXZpY29uLmljby9yb3V0ZVwiLFxuICAgICAgZnVuY3Rpb24gKCkge1xuICAgICAgICByZXR1cm4gcmVxdWlyZShcIi4vYXBwL2Zhdmljb24uaWNvXCIpO1xuICAgICAgfVxuICAgIF0pO1xuICAgIGlmKG1vZHVsZS5ob3QpIHtcbiAgICAgIG1vZHVsZS5ob3QuZGlzcG9zZShmdW5jdGlvbiAoKSB7XG4gICAgICAgIHdpbmRvdy5fX05FWFRfUC5wdXNoKFtcIi9mYXZpY29uLmljby9yb3V0ZVwiXSlcbiAgICAgIH0pO1xuICAgIH1cbiAgIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?absolutePagePath=%2Fhome%2Fesorokina%2Fwork%2Fstatfind%2Fapp%2Ffavicon.ico&page=%2Ffavicon.ico%2Froute!\n"));
+
+/***/ }),
+
+/***/ "(app-client)/./app/favicon.ico":
+/*!*************************!*\
+  !*** ./app/favicon.ico ***!
+  \*************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/favicon.603d046c.ico\",\"height\":16,\"width\":16,\"blurWidth\":0,\"blurHeight\":0});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vYXBwL2Zhdmljb24uaWNvLmpzIiwibWFwcGluZ3MiOiI7QUFBQSwrREFBZSxDQUFDLHFHQUFxRyIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvLi9hcHAvZmF2aWNvbi5pY28/YjM1MiJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XCJzcmNcIjpcIi9fbmV4dC9zdGF0aWMvbWVkaWEvZmF2aWNvbi42MDNkMDQ2Yy5pY29cIixcImhlaWdodFwiOjE2LFwid2lkdGhcIjoxNixcImJsdXJXaWR0aFwiOjAsXCJibHVySGVpZ2h0XCI6MH07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-client)/./app/favicon.ico\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("(app-client)/./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?absolutePagePath=%2Fhome%2Fesorokina%2Fwork%2Fstatfind%2Fapp%2Ffavicon.ico&page=%2Ffavicon.ico%2Froute!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
